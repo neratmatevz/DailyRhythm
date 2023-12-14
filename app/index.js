aaed38bed0527b12e438d8b005b926d338d768d7
neratmatevz <matevz.nerat@gmail.com>
2023-12-14 17:42:52 +0100

    First commit: base react-native

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { View, Text} from "react-native";
+
+const Home = () => {
+
+    return (
+        <View>
+            <Text>
+                DOMAČA STRAN
+            </Text>
+        </View>
+    )
+}
+
+
+export default Home;
